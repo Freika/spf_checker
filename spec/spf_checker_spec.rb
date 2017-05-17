b1b76be1d03bbf7ab37fbb2b6ff368e85ac9313c
Eugene Burmakin <iamfrey@gmail.com>
2017-05-17 17:18:43 +0700

    Add basic tests.

--- spec/spf_checker_spec.rb
+++ spec/spf_checker_spec.rb
@@ -5,7 +5,37 @@ RSpec.describe SpfChecker do
     expect(SpfChecker::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "Responses with an error if no valid domain provided" do
+    expect(SpfChecker.check('some string')).to eq(
+      {
+        correct: nil,
+        spf_value: nil,
+        message: "Please pass valid URI with protocol (\"http://\" or \"https://\")."
+      }
+    )
+  end
+
+  it "returns correct: true if SPF record is valid" do
+    expect(SpfChecker).to receive(:check).with("http://google.com/").and_return(
+      {
+        correct: true,
+        spf_value: [SpfChecker::VALID_VALUE],
+        message: "Request successfully complete."
+      }
+    )
+
+    SpfChecker.check("http://google.com/")
+  end
+
+  it "returns correct: false if SPF record is invalid" do
+    expect(SpfChecker).to receive(:check).with("http://non-existent.com/").and_return(
+      {
+        correct: false,
+        spf_value: [],
+        message: "Please pass valid URI with protocol (\"http://\" or \"https://\")."
+      }
+    )
+
+    SpfChecker.check("http://non-existent.com/")
   end
 end
