9ab94078125b51428f11869136d93048cf5c9285
Eugene Burmakin <iamfrey@gmail.com>
2017-05-17 14:38:42 +0700

    Check if passed URI is correct.

--- lib/spf_checker.rb
+++ lib/spf_checker.rb
@@ -1,17 +1,32 @@
 require 'spf_checker/version'
 require 'rubygems'
+require 'addressable/uri'
 require 'net/dns'
 
 module SpfChecker
   VALID_VALUE = 'v=spf1 a mx include:_spf.kiiiosk.ru ~all'.freeze
 
-  def self.correct(domain)
-    result = Net::DNS::Resolver.start(domain, Net::DNS::TXT)
-    values = result.each_mx.map(&:txt)
+  def self.check(uri)
+    if uri.include?('http://') || uri.include?('https://')
+      domain = Addressable::URI.parse(uri).host
+      message = 'Request successfully complete.'
 
+      result = Net::DNS::Resolver.start(domain, Net::DNS::TXT)
+      values = result.each_mx.map(&:txt)
+    else
+      message = 'Please pass valid URI with protocol ("http://" or "https://").'
+    end
+
+    response(message, values)
+  end
+
+  private
+
+  def self.response(message, values = [])
     {
-      correct: values.include?(VALID_VALUE),
-      spf_value: values
+      correct: values&.include?(VALID_VALUE),
+      spf_value: values,
+      message: message
     }
   end
 end
