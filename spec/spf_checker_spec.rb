8a7a9210e460d22e4c451fa2ee751e19d1467b71
Eugene Burmakin <iamfrey@gmail.com>
2017-05-18 19:06:12 +0700

    Refactor tests.

--- spec/spf_checker_spec.rb
+++ spec/spf_checker_spec.rb
@@ -1,28 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe SpfChecker do
-  let(:invalid_message) do
-    'Please pass valid URI with protocol ("http://" or "https://").'
-  end
+  let(:spf_value) { 'v=spf1 include:_spf.google.com ~all ' }
+  let(:checker) { SpfChecker::Domain.new(spf_value) }
 
   it 'has a version number' do
     expect(SpfChecker::VERSION).not_to be nil
   end
 
   it 'returns correct: true if SPF record is valid', :vcr do
-    VCR.use_cassette('test') do
-      spf_value = 'v=spf1 include:_spf.google.com ~all '
-      checker = SpfChecker::Domain.new(spf_value)
-      result = checker.check('google.com')
+    result = checker.check('google.com')
 
-      expect(result.correct).to be_truthy
-      expect(result.spf_value).to eq [spf_value]
-    end
+    expect(result.correct).to be_truthy
+    expect(result.spf_value).to eq [spf_value]
   end
 
   it 'returns correct: false if SPF record is invalid', :vcr do
-    spf_value = 'v=spf1 include:_spf.google.com ~all '
-    checker = SpfChecker::Domain.new(spf_value)
     result = checker.check('example.com')
 
     expect(result.correct).to be_falsey
